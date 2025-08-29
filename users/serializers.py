5126b595047bcae290ffd4ebf83af9dac1d54662
E <emilakbarov857@gmail.com>
2025-08-30 01:29:54 +0400

    users auth

--- users/serializers.py
+++ users/serializers.py
@@ -1,2 +1,82 @@
 from rest_framework import serializers
-from users.models import User,Profile
+from users.models import User, SuperHost, CityAndCountry, Language
+from .validators import validate_password
+
+
+class CityAncCountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CityAndCountry
+        fields = [
+            'city',
+            'country'
+        ]
+
+
+class LanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Language
+        fields = [
+            'language',
+        ]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    lives_in = CityAncCountrySerializer(read_only=True)
+    languages = LanguageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'email',
+            'phone',
+            'display_name',
+            'real_name',
+            'bio',
+            'lives_in',
+            'languages',
+            'website',
+            'twitter',
+            'facebook',
+            'instagram',
+            'profile_pic',
+            'cover',
+            'member_since'
+        ]
+
+
+class SuperHostSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = SuperHost
+        fields = [
+            'user',
+            'rating'
+        ]
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'}, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, style={'input_type': 'password'}, validators=[validate_password])
+
+    class Meta:
+        model = User
+        fields = [
+            'email',
+            'phone',
+            'display_name',
+            'real_name',
+            'password',
+            'password2'
+        ]
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError('Passwords must match')
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        password = validated_data.pop('password')
+        user = User.objects.create_user(password=password, **validated_data)
+        return user
--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
+from rest_framework.response import Response
 from django.shortcuts import render
+from rest_framework import generics, permissions, status
 
-# Create your views here.
+from .models import User, SuperHost
+from .serializers import UserRegisterSerializer, UserSerializer
+
+
+class UserRegisterAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response(
+            {
+                'message': 'User registered successfully',
+                'user': UserSerializer(user, context=self.get_serializer_context()).data
+            }
+            , status=status.HTTP_201_CREATED)
