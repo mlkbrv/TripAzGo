a6c104f22e3eb3c83d4379b32fb23b736644ad1e
E <emilakbarov857@gmail.com>
2025-09-02 15:23:05 +0400

    update 2

--- accomodations/permissions.py
+++ accomodations/permissions.py
@@ -17,15 +17,18 @@ class IsSuperHostOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        return request.user.is_authenticated and request.user.is_superhost
+        return request.user.is_authenticated and (request.user.is_superhost or request.user.is_superuser)
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        return request.user.is_authenticated and request.user.is_superhost and obj.owner == request.user
-
+        return (
+            request.user.is_authenticated
+            and (
+                request.user.is_superuser
+                or (request.user.is_superhost and obj.owner == request.user)
+            )
+        )
 
 class IsSuperHostOrAdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
